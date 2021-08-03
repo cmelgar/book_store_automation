9410cc2c4fd5998fb65c4183cd747db354ab9b37
cmelgar <carlosaquijemelgar@gmail.com>
2021-08-02 22:15:02 -0500

    getting url from yaml file

--- src/test/java/Hooks.java
+++ src/test/java/Hooks.java
@@ -4,7 +4,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import utils.YAMLHelper;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,10 +14,12 @@ import java.util.Map;
 public class Hooks {
 
     public static WebDriver driver;
+    YAMLHelper yamlHelper;
 
     @Before
-    public void setup() {
+    public void setup() throws IOException {
 
+        yamlHelper = new YAMLHelper();
         Map<String, Object> prefs = new HashMap<>();
 
         prefs.put("profile.default_content_setting_values.notifications", 2);
@@ -29,7 +33,7 @@ public class Hooks {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
 
-        String url = "https://www.ebay.com/";
+        String url = yamlHelper.getProperty("url");
         driver.get(url);
     }
 
---
+++ src/test/java/utils/YAMLHelper.java
@@ -0,0 +1,18 @@
+package utils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+public class YAMLHelper {
+
+    public String getProperty(String property) throws IOException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        JsonNode jsonNode = mapper.readValue(new File("src/test/resources/configuration.yaml"), JsonNode.class);
+        JsonNode object = jsonNode.get(1);
+        return object.get(property).textValue();
+    }
+}
